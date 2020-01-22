164e38a6b1bef95934df307c56232044666dd2df
Juan D. Bermudez <juan.bermudez1102@outlook.com>
2020-01-21 22:08:58 -0500

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
+const num1 = 31
+const num2 = 2;
 
-// Write your code here
+const multiply = num1 * num2;
+const random = Math.floor(Math.random() * 10) + 1;
+const mod = 40 % 9;
+const max = Math.max(1,10,20,0,17);
