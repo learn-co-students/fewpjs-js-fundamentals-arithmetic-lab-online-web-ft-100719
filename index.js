9b0fc16dd434251213eab97787804dab8f7fb3ac
yasmine-hj <yasminehajaji95@gmail.com>
2020-01-24 12:44:15 +0000

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 
 // Write your code here
+
+const num1 = 2
+const num2 = 31
+const multiply = num1 * num2
+const random = Math.floor(Math.random())+100;
+const mod = 40%9
+const max = Math.max(1, 2, 20, 9, 0, 18);
+
+
