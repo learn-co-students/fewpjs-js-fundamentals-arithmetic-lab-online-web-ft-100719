f3b729739660568fd1409aa1604748e990d24824
Emilia <emilia.mena26@gmail.com>
2020-02-02 19:30:38 -0500

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
-
 // Write your code here
+let num1 = 31
+let num2 = 2
+var multiply = num1 * num2
+
+var random = Math.floor(Math.random() * 10) + 1
+
+var mod = 25 % 7
+
+var max = Math.max(1, 5, 15, 20)
