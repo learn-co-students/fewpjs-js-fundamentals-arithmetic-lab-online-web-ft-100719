d4774bb0d768acdfe0c1b694327e2ef89bdcddba
Huda Yousif <hjoski@gmail.com>
2020-02-02 22:02:35 -0800

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 
 // Write your code here
+
+let num1 = 31
+let num2 = 2
+
+let multiply = num1 * num2;
+let random = Math.floor(Math.random() * 10) + 1;
+let mod = 19 % 5;
+let max = Math.max(1, 5, 10, 12, 20);
